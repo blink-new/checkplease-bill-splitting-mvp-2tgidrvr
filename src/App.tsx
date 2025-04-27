363bf184097b7d47b4f54a620fe89263933e78e0
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-27 08:25:32 +0000

    Set Up React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { useState } from 'react'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import HomePage from './pages/HomePage';
+import CreateSessionPage from './pages/CreateSessionPage';
+import SessionPage from './pages/SessionPage';
+import JoinSessionPage from './pages/JoinSessionPage';
+import NotFoundPage from './pages/NotFoundPage';
+import { ThemeProvider } from './components/ThemeProvider';
 
 function App() {
   return (
-    <div></div>
-  )
+    <ThemeProvider defaultTheme="light" storageKey="checkplease-theme">
+      <Router>
+        <Toaster position="top-center" />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreateSessionPage />} />
+          <Route path="/session/:billId" element={<SessionPage />} />
+          <Route path="/join/:billId" element={<JoinSessionPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App 
+export default App;
